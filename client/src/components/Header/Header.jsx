2f260710dd02a7d080371d1b44094d9ead288168
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2023-04-02 11:26:42 +0530

    ongoing

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState, useContext} from "react";
+import { useNavigate} from "react-router-dom";
+
+import {TbSearch} from "react-icons/tb";
+import {CgShoppingCart} from "react-icons/cg";
+import {AiOutlineHeart} from "react-icons/ai";
+
+import Search from "./Search/Search";
+import Cart from "./Search/Search";
+import {Context} from "../../utils/context";
+
+import "./Header.scss";
+
+const Header = () => {
+    return <div>Header</div>;
+};
+
+export default Header;
